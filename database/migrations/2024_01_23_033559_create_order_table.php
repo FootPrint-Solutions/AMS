28133671877a457e577d6918cdd80d9053079157
William Oktavianus <williamoktavianus1099@gmail.com>
2024-01-23 11:10:34 +0700

    Adding another DB migration
    Add DB migration for Order, Trade In, Pricing Strategy and Mechanic.

---
+++ database/migrations/2024_01_23_033559_create_order_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->unsignedBigInteger('id_customer');
+            $table->unsignedBigInteger('id_battery');
+            $table->string('battery_production_code');
+            $table->integer('battery_quantity');
+            $table->boolean('battery_trade_in');
+            $table->boolean('installation');
+            $table->unsignedBigInteger('id_mechanic');
+            $table->double('cost_delivery');
+            $table->double('price_total');
+            $table->boolean('status');
+            $table->timestamps();
+
+            $table->foreign('id_customer')
+                ->references('id')
+                ->on('customer');
+            $table->foreign('id_battery')
+                ->references('id')
+                ->on('battery');
+            $table->foreign('id_mechanic')
+                ->references('id')
+                ->on('mechanic');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order');
+    }
+}
