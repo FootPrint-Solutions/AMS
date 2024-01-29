1be3996afbae42b0089170681088f30136761dfb
William Oktavianus <williamoktavianus1099@gmail.com>
2024-01-29 12:24:10 +0700

    Moving script dependencies in master

--- resources/views/template/master.blade.php
+++ resources/views/template/master.blade.php
@@ -1,101 +1,89 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
+  <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
     <title>{{ $title ?? '' }}</title>
 
-    <!-- Favicon -->
+    {{-- Favicon --}}
     <link rel="shortcut icon" href="assets/img/favicon.png">
 
-    <!-- Fontfamily -->
+    {{-- Fontfamily --}}
     <link
         href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;0,900;1,400;1,500;1,700&amp;display=swap"
         rel="stylesheet">
 
-    <!-- Bootstrap CSS -->
+    {{-- Bootstrap CSS --}}
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
 
-    <!-- Feathericon CSS -->
+    {{-- Feathericon CSS --}}
     <link rel="stylesheet" href="{{ asset('plugins/feather/feather.css') }}">
 
-    <!-- Pe7 CSS -->
-    <link rel="stylesheet" href="{{ asset('/css/flags.css') }}">
-
-    <!-- Toatr CSS -->		
+    {{-- Toatr CSS	 --}}
     <link rel="stylesheet" href=" {{ asset('plugins//toastr/toatr.css') }}">
 
-    <!-- Fontawesome CSS -->
+    {{-- Fontawesome CSS --}}
     <link rel="stylesheet" href="{{ asset('/plugins/fontawesome/css/fontawesome.min.css') }}">
     <link rel="stylesheet" href="{{ asset('/plugins/fontawesome/css/all.min.css') }}">
 
-    <!-- Main CSS -->
-    <link rel="stylesheet" href="{{ asset('/css/style.css') }}">
-</head>
-
-<body>
-
-    <!-- Main Wrapper -->
-    <div class="main-wrapper">
-
-        @include('template.header')
-        @include('template.sidebar')
-
-
-        <!-- Page Wrapper -->
-        <div class="page-wrapper mb-5">
-            <div class="content container-fluid">
-                @yield('content')
-            </div>
-
-            @include('template.footer')
-
-        </div>
-        <!-- /Page Wrapper -->
-
-
-
-    </div>
-    <!-- /Main Wrapper -->
-
-    <!-- jQuery -->
+    {{-- jQuery --}}
     <script src="{{ asset('/js/jquery-3.7.1.min.js') }}"></script>
 
-    <!-- Bootstrap Core JS -->
+    {{-- Bootstrap Core JS --}}
     <script src="{{ asset('/js/bootstrap.bundle.min.js') }}"></script>
 
-    <!-- Feather Icon JS -->
+    {{-- Feather Icon JS --}}
     <script src="{{ asset('/js/feather.min.js') }}"></script>
 
-    <!-- Slimscroll JS -->
+    {{-- Slimscroll JS --}}
     <script src="{{ asset('/plugins/slimscroll/jquery.slimscroll.min.js') }}"></script>
 
-    <!-- Chart JS -->
+    {{-- Chart JS --}}
     <script src="{{ asset('/plugins/apexchart/apexcharts.min.js') }}"></script>
     <script src="{{ asset('/plugins/apexchart/chart-data.js') }}"></script>
 
-    <!-- Mask JS -->
-		<script src="{{ asset('/plugins/toastr/toastr.min.js') }}"></script>
+    {{-- Mask JS --}}
+    <script src="{{ asset('/plugins/toastr/toastr.min.js') }}"></script>
 
-    <!-- Custom JS -->
+    {{-- Custom JS --}}
     <script src="{{ asset('/js/script.js') }}"></script>
 
-</body>
+    {{-- Main CSS --}}
+    <link rel="stylesheet" href="{{ asset('/css/style.css') }}">
+  </head>
 
+  <body>
+    <div class="main-wrapper">
+      {{-- Header --}}
+      @include('template.header')
+
+      {{-- Sidebar --}}
+      @include('template.sidebar')
+
+      {{-- Content --}}
+      <div class="page-wrapper mb-5">
+          <div class="content container-fluid">
+              @yield('content')
+          </div>
+
+          @include('template.footer')
+      </div>
+    </div>
+  </body>
+
+  <script>
+    function showSuccessToast(message) {
+      toastr.success(message, {
+        closeButton: true,
+        tapToDismiss: !1,
+      });
+    }
+
+    function showErrorToast(message) {
+      toastr.error(message, {
+        closeButton: !0,
+        tapToDismiss: !1,
+      });
+    }
+  </script>
 </html>
-<script>
-  function showSuccessToast(message) {
-    toastr.success(message, {
-      closeButton: true,
-      tapToDismiss: !1,
-    });
-  }
-
-  function showErrorToast(message) {
-    toastr.error(message, {
-      closeButton: !0,
-      tapToDismiss: !1,
-    });
-  }
-</script>
