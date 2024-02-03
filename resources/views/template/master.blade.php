6de4db9ac9dfe5a30bb5e7f137f1be25f5881f4e
R4D3N G0Z4LL <radengozal@gmail.com>
2024-02-03 16:05:07 +0700

    Update quotation, baru bisa send wa dari Personal Details

--- resources/views/template/master.blade.php
+++ resources/views/template/master.blade.php
@@ -1,129 +1,130 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
-    <title>{{ $title ?? '' }}</title>
 
-    {{-- Favicon --}}
-    {{-- <link rel="shortcut icon" href="assets/img/favicon.png"> --}}
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <title>{{ $title ?? '' }}</title>
 
-    {{-- Fontfamily --}}
-    <link
-        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;0,900;1,400;1,500;1,700&amp;display=swap"
-        rel="stylesheet">
+  {{-- Favicon --}}
+  {{-- <link rel="shortcut icon" href="assets/img/favicon.png"> --}}
 
-    {{-- Bootstrap CSS --}}
-    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+  {{-- Fontfamily --}}
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;0,900;1,400;1,500;1,700&amp;display=swap" rel="stylesheet">
 
-    {{-- Feathericon CSS --}}
-    <link rel="stylesheet" href="{{ asset('plugins/feather/feather.css') }}">
+  {{-- Bootstrap CSS --}}
+  <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
 
-    {{-- Toatr CSS --}}
-    <link rel="stylesheet" href=" {{ asset('plugins//toastr/toatr.css') }}">
+  {{-- Feathericon CSS --}}
+  <link rel="stylesheet" href="{{ asset('plugins/feather/feather.css') }}">
 
-    {{-- Fontawesome CSS --}}
-    <link rel="stylesheet" href="{{ asset('/plugins/fontawesome/css/fontawesome.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('/plugins/fontawesome/css/all.min.css') }}">
+  {{-- Toatr CSS --}}
+  <link rel="stylesheet" href=" {{ asset('plugins//toastr/toatr.css') }}">
 
-    {{-- Datatables CSS --}}
-		<link rel="stylesheet" href="{{ asset('/plugins/datatables/datatables.min.css') }}">
+  {{-- Fontawesome CSS --}}
+  <link rel="stylesheet" href="{{ asset('/plugins/fontawesome/css/fontawesome.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('/plugins/fontawesome/css/all.min.css') }}">
 
-    {{-- Select2 CSS --}}
-    <link rel="stylesheet" href="{{ asset('/plugins/select2/css/select2.min.css') }}">
+  {{-- Datatables CSS --}}
+  <link rel="stylesheet" href="{{ asset('/plugins/datatables/datatables.min.css') }}">
 
-    {{-- Main CSS --}}
-    <link rel="stylesheet" href="{{ asset('/css/style.css') }}">
+  {{-- Select2 CSS --}}
+  <link rel="stylesheet" href="{{ asset('/plugins/select2/css/select2.min.css') }}">
 
-    {{-- jQuery --}}
-    <script src="{{ asset('/js/jquery-3.7.1.min.js') }}"></script>
-  </head>
+  {{-- Main CSS --}}
+  <link rel="stylesheet" href="{{ asset('/css/style.css') }}">
 
-  <body>
-    <div id="main-wrapper">
-      {{-- Header --}}
-      @include('template.header')
+  {{-- jQuery --}}
+  <script src="{{ asset('/js/jquery-3.7.1.min.js') }}"></script>
+</head>
 
-      {{-- Sidebar --}}
-      @include('template.sidebar')
+<body>
+  <div id="main-wrapper">
+    {{-- Header --}}
+    @include('template.header')
 
-      {{-- Content --}}
-      <div class="page-wrapper mb-5">
-          <div class="content container-fluid">
-              @yield('content')
-          </div>
+    {{-- Sidebar --}}
+    @include('template.sidebar')
 
-          @include('template.footer')
+    {{-- Content --}}
+    <div class="page-wrapper mb-5">
+      <div class="content container-fluid">
+        @yield('content')
       </div>
+
+      @include('template.footer')
     </div>
+  </div>
 
-    {{-- Bootstrap Core JS --}}
-    <script src="{{ asset('/js/bootstrap.bundle.min.js') }}"></script>
+  {{-- Bootstrap Core JS --}}
+  <script src="{{ asset('/js/bootstrap.bundle.min.js') }}"></script>
 
-    {{-- Feather Icon JS --}}
-    <script src="{{ asset('/js/feather.min.js') }}"></script>
+  {{-- Feather Icon JS --}}
+  <script src="{{ asset('/js/feather.min.js') }}"></script>
 
-    {{-- Slimscroll JS --}}
-    <script src="{{ asset('/plugins/slimscroll/jquery.slimscroll.min.js') }}"></script>
+  {{-- Slimscroll JS --}}
+  <script src="{{ asset('/plugins/slimscroll/jquery.slimscroll.min.js') }}"></script>
 
-    {{-- Chart JS --}}
-    <script src="{{ asset('/plugins/apexchart/apexcharts.min.js') }}"></script>
-    <script src="{{ asset('/plugins/apexchart/chart-data.js') }}"></script>
+  {{-- Chart JS --}}
+  <script src="{{ asset('/plugins/apexchart/apexcharts.min.js') }}"></script>
+  <script src="{{ asset('/plugins/apexchart/chart-data.js') }}"></script>
 
-    {{-- Toastr JS --}}
-    <script src="{{ asset('/plugins/toastr/toastr.min.js') }}"></script>
+  {{-- Toastr JS --}}
+  <script src="{{ asset('/plugins/toastr/toastr.min.js') }}"></script>
 
-    {{-- Datatables JS --}}
-		<script src="{{ asset('/plugins/datatables/datatables.min.js') }}"></script>
+  {{-- Datatables JS --}}
+  <script src="{{ asset('/plugins/datatables/datatables.min.js') }}"></script>
 
-    {{-- Select2 JS --}}
-    <script src="{{ asset('/plugins/select2/js/select2.min.js') }}"></script>
+  {{-- Select2 JS --}}
+  <script src="{{ asset('/plugins/select2/js/select2.min.js') }}"></script>
 
-    {{-- Sweetalert JS --}}
-    <script src="{{ asset('/plugins/sweetalert/sweetalerts.min.js') }}"></script>
-    <script src="{{ asset('/plugins/sweetalert/sweetalert2.all.min.js') }}"></script>
+  {{-- Sweetalert JS --}}
+  <script src="{{ asset('/plugins/sweetalert/sweetalerts.min.js') }}"></script>
+  <script src="{{ asset('/plugins/sweetalert/sweetalert2.all.min.js') }}"></script>
 
-    {{-- Custom JS --}}
-    <script src="{{ asset('/js/script.js') }}"></script>
-  </body>
+  {{-- Custom JS --}}
+  <script src="{{ asset('/js/script.js') }}"></script>
+</body>
 
-  <script>
-    /**
-     * Go to a certain view by replacing main-wrapper (to achieve SPA functionality).
-     *
-     * @param {string} destination - The destination view
-     */
-    function goToPage(destination) {
-      $.ajax({
-        url: destination,
-        success: function(response) {
-            $('#main-wrapper').html(response);
-        }
-      });
-    }
+<script>
+  /**
+   * Go to a certain view by replacing main-wrapper (to achieve SPA functionality).
+   *
+   * @param {string} destination - The destination view
+   */
+  function goToPage(destination) {
+    $.ajax({
+      url: destination,
+      success: function(response) {
+        $('#main-wrapper').html(response);
+      }
+    });
+  }
 
-    /**
-     * Displays a success toast message using Toastr.
-     *
-     * @param {string} message - The success message to be displayed.
-     */
-    function showSuccessToast(message) {
-      toastr.success(message, {
-        closeButton: true,
-        tapToDismiss: !1,
-      });
-    }
+  /**
+   * Displays a success toast message using Toastr.
+   *
+   * @param {string} message - The success message to be displayed.
+   */
+  function showSuccessToast(message) {
+    toastr.success(message, {
+      closeButton: true,
+      tapToDismiss: !1,
+    });
+  }
 
-    /**
-     * Displays an error toast message using Toastr.
-     *
-     * @param {string} message - The error message to be displayed.
-     */
-    function showErrorToast(message) {
-      toastr.error(message, {
-        closeButton: !0,
-        tapToDismiss: !1,
-      });
-    }
-  </script>
+  /**
+   * Displays an error toast message using Toastr.
+   *
+   * @param {string} message - The error message to be displayed.
+   */
+  function showErrorToast(message) {
+    toastr.error(message, {
+      closeButton: !0,
+      tapToDismiss: !1,
+    });
+  }
+</script>
+
 </html>
