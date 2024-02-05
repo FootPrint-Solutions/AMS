66edfa57162606287e3e6c1da028c9524cf23d3f
William Oktavianus <williamoktavianus1099@gmail.com>
2024-02-05 09:46:14 +0700

    Adding Menu Sub migration

---
+++ database/seeders/MenuSubSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class MenuSubSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Clear existing data in menu table.
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+        DB::table('menu_sub')->truncate();
+        DB::statement('SET FOREIGN_KEY_CHECKS=1');
+
+        // Insert menu data to menu table.
+        DB::table('menu_sub')->insert([
+            ['name' => 'Brand', 'id_menu' => 3, 'order' => 1, 'url' => '/vehicle/brand', 'hide' => 0, 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Brand', 'id_menu' => 4, 'order' => 1, 'url' => '/battery/brand', 'hide' => 0, 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Subbrand Category', 'id_menu' => 4, 'order' => 2, 'url' => '/battery/subbrand', 'hide' => 0, 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Usage Type', 'id_menu' => 4, 'order' => 3, 'url' => '/battery/usage', 'hide' => 0, 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Technology', 'id_menu' => 4, 'order' => 4, 'url' => '/battery/technology', 'hide' => 0, 'created_at' => now(), 'updated_at' => now()],
+        ]);
+    }
+}
