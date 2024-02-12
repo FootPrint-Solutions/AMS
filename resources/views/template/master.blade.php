9df6b905c14d923a4e5c81db0a120ac05285acb5
William Oktavianus <williamoktavianus1099@gmail.com>
2024-02-12 11:04:09 +0700

    Adding loading indicator

--- resources/views/template/master.blade.php
+++ resources/views/template/master.blade.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <title>{{ $title ?? '' }}</title>
+  <title>{{ $title ?? "" }}</title>
 
   {{-- Favicon --}}
   {{-- <link rel="shortcut icon" href="assets/img/favicon.png"> --}}
@@ -14,84 +14,91 @@
   <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;0,900;1,400;1,500;1,700&amp;display=swap" rel="stylesheet">
 
   {{-- Bootstrap CSS --}}
-  <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+  <link rel="stylesheet" href="{{ asset("css/bootstrap.min.css") }}">
 
   {{-- Feathericon CSS --}}
-  <link rel="stylesheet" href="{{ asset('plugins/feather/feather.css') }}">
+  <link rel="stylesheet" href="{{ asset("plugins/feather/feather.css") }}">
 
   {{-- Toatr CSS --}}
-  <link rel="stylesheet" href=" {{ asset('plugins//toastr/toatr.css') }}">
+  <link rel="stylesheet" href=" {{ asset("plugins//toastr/toatr.css") }}">
 
   {{-- Fontawesome CSS --}}
-  <link rel="stylesheet" href="{{ asset('/plugins/fontawesome/css/fontawesome.min.css') }}">
-  <link rel="stylesheet" href="{{ asset('/plugins/fontawesome/css/all.min.css') }}">
+  <link rel="stylesheet" href="{{ asset("/plugins/fontawesome/css/fontawesome.min.css") }}">
+  <link rel="stylesheet" href="{{ asset("/plugins/fontawesome/css/all.min.css") }}">
 
   {{-- Datatables CSS --}}
-  <link rel="stylesheet" href="{{ asset('/plugins/datatables/datatables.min.css') }}">
-
+  <link rel="stylesheet" href="{{ asset("/plugins/datatables/datatables.min.css") }}">
 
   {{-- Select2 CSS --}}
-  <link rel="stylesheet" href="{{ asset('/plugins/select2/css/select2.min.css') }}">
+  <link rel="stylesheet" href="{{ asset("/plugins/select2/css/select2.min.css") }}">
 
-  {{-- Main CSS --}}
-  <link rel="stylesheet" href="{{ asset('/css/style.css') }}">
+  {{-- Main Preschool CSS --}}
+  <link rel="stylesheet" href="{{ asset("/css/style.css") }}">
+
+  {{-- Personal CSS --}}
+  <link rel="stylesheet" href="{{ asset("/css/app.css") }}">
 
   {{-- jQuery --}}
-  <script src="{{ asset('/js/jquery-3.7.1.min.js') }}"></script>
+  <script src="{{ asset("/js/jquery-3.7.1.min.js") }}"></script>
 </head>
 
 <body>
   <div id="main-wrapper">
     {{-- Header --}}
-    @include('template.header')
+    @include("template.header")
 
     {{-- Sidebar --}}
-    @include('template.sidebar')
+    @include("template.sidebar")
 
     {{-- Content --}}
     <div class="page-wrapper mb-5">
       <div class="content container-fluid">
-        @yield('content')
+        {{-- Loading Indicator --}}
+        <div class="spinner-border text-primary" id="loading-indicator">
+          <span class="sr-only">Loading...</span>
+        </div>
+
+        @yield("content")
       </div>
 
-      @include('template.footer')
+      @include("template.footer")
     </div>
   </div>
 
   {{-- Bootstrap Core JS --}}
-  <script src="{{ asset('/js/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ asset("/js/bootstrap.bundle.min.js") }}"></script>
 
   {{-- Feather Icon JS --}}
-  <script src="{{ asset('/js/feather.min.js') }}"></script>
+  <script src="{{ asset("/js/feather.min.js") }}"></script>
 
   {{-- Slimscroll JS --}}
-  <script src="{{ asset('/plugins/slimscroll/jquery.slimscroll.min.js') }}"></script>
+  <script src="{{ asset("/plugins/slimscroll/jquery.slimscroll.min.js") }}"></script>
 
   {{-- Chart JS --}}
-  <script src="{{ asset('/plugins/apexchart/apexcharts.min.js') }}"></script>
-  <script src="{{ asset('/plugins/apexchart/chart-data.js') }}"></script>
+  <script src="{{ asset("/plugins/apexchart/apexcharts.min.js") }}"></script>
+  <script src="{{ asset("/plugins/apexchart/chart-data.js") }}"></script>
 
   {{-- Toastr JS --}}
-  <script src="{{ asset('/plugins/toastr/toastr.min.js') }}"></script>
+  <script src="{{ asset("/plugins/toastr/toastr.min.js") }}"></script>
 
   {{-- Datatables JS --}}
-  <script src="{{ asset('/plugins/datatables/datatables.min.js') }}"></script>
-  <script src="{{ asset('/plugins/datatables/dataTables.buttons.min.js') }}"></script>
-  <script src="{{ asset('/plugins/datatables/buttons.html5.min.js') }}"></script>
-  <script src="{{ asset('/plugins/datatables/jszip.min.js') }}"></script>
-  <script src="{{ asset('/plugins/datatables/pdfmake.min.js') }}"></script>
-  <script src="{{ asset('/plugins/datatables/vfs_fonts.js') }}"></script>
+  <script src="{{ asset("/plugins/datatables/datatables.min.js") }}"></script>
+  <script src="{{ asset("/plugins/datatables/dataTables.buttons.min.js") }}"></script>
+  <script src="{{ asset("/plugins/datatables/buttons.html5.min.js") }}"></script>
+  <script src="{{ asset("/plugins/datatables/jszip.min.js") }}"></script>
+  <script src="{{ asset("/plugins/datatables/pdfmake.min.js") }}"></script>
+  <script src="{{ asset("/plugins/datatables/vfs_fonts.js") }}"></script>
 
 
   {{-- Select2 JS --}}
-  <script src="{{ asset('/plugins/select2/js/select2.min.js') }}"></script>
+  <script src="{{ asset("/plugins/select2/js/select2.min.js") }}"></script>
 
   {{-- Sweetalert JS --}}
-  <script src="{{ asset('/plugins/sweetalert/sweetalerts.min.js') }}"></script>
-  <script src="{{ asset('/plugins/sweetalert/sweetalert2.all.min.js') }}"></script>
+  <script src="{{ asset("/plugins/sweetalert/sweetalerts.min.js") }}"></script>
+  <script src="{{ asset("/plugins/sweetalert/sweetalert2.all.min.js") }}"></script>
 
   {{-- Custom JS --}}
-  <script src="{{ asset('/js/script.js') }}"></script>
+  <script src="{{ asset("/js/script.js") }}"></script>
 </body>
 
 <script>
@@ -103,8 +110,12 @@
   function goToPage(destination) {
     $.ajax({
       url: destination,
+      beforeSend: function () {
+        $("#loading-indicator").show();
+      },
       success: function(response) {
-        $('#main-wrapper').html(response);
+        $("#loading-indicator").hide();
+        $("#main-wrapper").html(response);
       }
     });
   }
