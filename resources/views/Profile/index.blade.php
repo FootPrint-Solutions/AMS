bb17c7f7a3673385754107545ed0c940dc7d23f8
William Oktavianus <williamoktavianus1099@gmail.com>
2024-02-16 09:02:44 +0700

    Moving about tab script
    Move about tab script to a new file (separated per tab).

--- resources/views/Profile/index.blade.php
+++ resources/views/Profile/index.blade.php
@@ -3,6 +3,7 @@
 @section('content')
     <div class="row">
         <div class="col-md-12">
+            {{-- Header --}}
             <div class="profile-header">
                 <div class="row align-items-center">
                     <div class="col-auto profile-image">
@@ -10,12 +11,14 @@
                             <img class="rounded-circle" alt="User Image" src="{{ asset('/img/profiles/avatar-02.jpg') }}">
                         </a>
                     </div>
+
                     <div class="col ms-md-n2 profile-user-info">
                         @auth
                             <h4 class="user-name mb-0">{{ auth()->user()->name }}</h4>
                             <h6 class="text-muted">{{ auth()->user()->email }}</h6>
                         @endauth
                     </div>
+
                     <div class="col-auto profile-btn">
                         <a href="" class="btn btn-primary">
                             Edit Profile Picture
@@ -23,148 +26,35 @@
                     </div>
                 </div>
             </div>
+
+            {{-- Content Title --}}
             <div class="profile-menu">
                 <ul class="nav nav-tabs nav-tabs-solid">
+                    {{-- About --}}
                     <li class="nav-item">
-                        <a class="nav-link active" data-bs-toggle="tab" href="#per_details_tab">About</a>
+                        <a class="nav-link active" data-bs-toggle="tab" href="#about-tab">About</a>
                     </li>
+
+                    {{-- Password --}}
                     <li class="nav-item">
-                        <a class="nav-link" data-bs-toggle="tab" href="#password_tab">Password</a>
+                        <a class="nav-link" data-bs-toggle="tab" href="#password-tab">Password</a>
                     </li>
                 </ul>
             </div>
+
+            {{-- Content Tab --}}
             <div class="tab-content profile-tab-cont">
-
-                <!-- Personal Details Tab -->
-                <div class="tab-pane fade show active" id="per_details_tab">
-
-                    <!-- Personal Details -->
-                    <div class="row">
-                        <div class="col-lg-9">
-                            <div class="card">
-                                <div class="card-body">
-                                    <h5 class="card-title d-flex justify-content-between">
-                                        <span>Personal Details</span>
-                                        <a class="edit-link" data-bs-toggle="modal" href="#edit_personal_details"><i
-                                                class="far fa-edit me-1"></i>Edit</a>
-                                    </h5>
-                                    <div class="row">
-                                        <p class="col-sm-3 text-muted text-sm-end mb-0 mb-sm-3">Name</p>
-                                        @auth
-                                            <p class="col-sm-9">{{ auth()->user()->name }}</p>
-                                        @endauth
-
-                                    </div>
-
-                                    <div class="row">
-                                        <p class="col-sm-3 text-muted text-sm-end mb-0 mb-sm-3">Email ID</p>
-                                        @auth
-                                            <p class="col-sm-9">{{ auth()->user()->email }}</p>
-                                        @endauth
-
-                                    </div>
-
-                                </div>
-                            </div>
-
-                        </div>
-
-                        <div class="col-lg-3">
-
-                            <!-- Account Status -->
-                            <div class="card">
-                                <div class="card-body">
-                                    <h5 class="card-title d-flex justify-content-between">
-                                        <span>Whatsapp Status</span>
-                                        <a class="edit-link" href=""><i class="fa-solid fa-arrows-rotate"></i></a>
-                                    </h5>
-
-                                    @if ($data['QrCode'] != '')
-                                        <badge class="badge bg-danger">Not Active, Please Scan here</badge>
-                                        <img src="{{ $data['QrCode'] }}" alt="QrCode" class="img-fluid">
-                                    @else
-                                        <badge class="badge bg-success">Active</badge>
-                                        <br>
-                                        <button class="btn btn-success btn-lg mt-1" id='DeleteSessionWhatsApp'
-                                            type="button"><i class="fe fe-check-verified"></i>
-                                            <i class="fa-solid fa-qrcode"></i></button>
-                                    @endif
-
-
-                                </div>
-                            </div>
-                            <!-- /Account Status -->
-
-
-
-                        </div>
-                    </div>
-                    <!-- /Personal Details -->
-
+                {{-- About Tab --}}
+                <div class="tab-pane fade show active" id="about-tab">
+                    @include('Profile.about')
                 </div>
-                <!-- /Personal Details Tab -->
 
                 {{-- Password Tab --}}
-                <div id="password_tab" class="tab-pane fade">
+                <div id="password-tab" class="tab-pane fade">
                     @include('Profile.password')
                 </div>
 
             </div>
         </div>
     </div>
-
-    <script>
-        $('#DeleteSessionWhatsApp').click(function() {
-            swal.fire({
-                title: "Are you sure?",
-                text: "Once deleted, you will need to scan the QR code again to activate the session",
-                icon: "warning",
-                showCancelButton: true,
-                confirmButtonColor: "#3085d6",
-                cancelButtonColor: "#d33",
-                confirmButtonText: "Yes, delete it!",
-                confirmButtonClass: "btn btn-primary",
-                cancelButtonClass: "btn btn-danger ml-1",
-                buttonsStyling: !1
-            }).then((willDelete) => {
-                if (willDelete.isConfirmed) {
-                    $.ajax({
-                        url: "/delete-session-whatsapp",
-                        type: 'GET',
-                        success: function(data) {
-                            let ResponseData = JSON.parse(data);
-                            if (ResponseData.status) {
-                                swal.fire({
-                                    title: "Success",
-                                    text: ResponseData.message,
-                                    icon: "success",
-                                });
-                                location.reload();
-                            } else {
-                                swal.fire({
-                                    title: "Error",
-                                    text: ResponseData.message ||
-                                        "Something went wrong, please try again later",
-                                    icon: "error",
-                                });
-                            }
-                        },
-                        error: function() {
-                            swal.fire({
-                                title: "Error",
-                                text: "Something went wrong",
-                                icon: "error",
-                            });
-                        }
-                    });
-                } else {
-                    swal.fire({
-                        title: "Cancelled",
-                        text: "Your session is safe",
-                        icon: "error",
-                    });
-                }
-            });
-        });
-    </script>
 @endsection
