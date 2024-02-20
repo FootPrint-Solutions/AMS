725536cd9a5387bbc5dc92602dc4d0229a8c5734
William Oktavianus <williamoktavianus1099@gmail.com>
2024-02-20 16:02:50 +0700

    Adding database migration
    Add database migration for partners (distributor, shop and technician).

---
+++ database/migrations/distributor/2024_02_20_082324_create_distributor_shop_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDistributorShopTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('distributor_shop', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('id_distributor');
+            $table->string('address');
+            $table->point('coordinate');
+            $table->string('contact_person');
+            $table->string('contact');
+            $table->string('email');
+            $table->string('note')->nullable();
+            $table->timestamps();
+
+            /*
+            $table->foreign('id_distributor')
+                ->references('id')
+                ->on('distributor');
+            */
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('distributor_shop');
+    }
+}
