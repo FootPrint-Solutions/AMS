6484fe1072220c8bd01f00db9695ee573e3b78de
William Oktavianus <williamoktavianus1099@gmail.com>
2024-02-21 08:55:26 +0700

    Adding models and controllers for Partner
    Add new models and controllers (empty) for Distributor, Shops and Technician.

---
+++ app/Models/MasterData/Distributor/DistributorShopModel.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models\MasterData\Distributor;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class DistributorShopModel extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'distributor_shop';
+}
