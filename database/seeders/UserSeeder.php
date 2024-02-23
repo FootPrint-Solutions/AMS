19edc5072688c879a2f57bb636a33314da631799
R4D3N G0Z4LL <radengozal@gmail.com>
2024-02-23 10:12:28 +0700

    Move seeder customer and user

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders\users;
+
+use Illuminate\Database\Seeder;
+
+// MODELS
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::factory(10)->create();
+    }
+}
