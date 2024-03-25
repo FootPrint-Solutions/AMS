86e6bbc052787d155e08909080d3b47f7ae527d8
William Oktavianus <williamoktavianus1099@gmail.com>
2024-03-25 16:21:47 +0700

    Fixing minor error in migration

--- database/migrations/sales_orders_view/2024_03_25_091457_create_sales_orders_view.php
+++ database/migrations/sales_orders_view/2024_03_25_091457_create_sales_orders_view.php
@@ -13,12 +13,12 @@ class CreateSalesOrdersView extends Migration
     public function up()
     {
         DB::statement("CREATE VIEW sales_orders_view AS
-                        SELECT quotations.*, customers.name AS customer_name, shops.name AS shop_name, distributors.id AS distributor_id, distributors.name AS distributor_name, technicians.name AS technician_name
-                        FROM quotations
-                            LEFT JOIN customers ON quotations.customer_id = customers.id
-                            LEFT JOIN distributor_shops AS shops ON quotations.distributor_shop_id = shops.id
+                        SELECT sales_orders.*, customers.name AS customer_name, shops.name AS shop_name, distributors.id AS distributor_id, distributors.name AS distributor_name, technicians.name AS technician_name
+                        FROM sales_orders
+                            LEFT JOIN customers ON sales_orders.customer_id = customers.id
+                            LEFT JOIN distributor_shops AS shops ON sales_orders.distributor_shop_id = shops.id
                             LEFT JOIN distributors ON shops.distributor_id = distributors.id
-                            LEFT JOIN distributor_shop_technicians AS technicians ON quotations.distributor_shop_technician_id = technicians.id");
+                            LEFT JOIN distributor_shop_technicians AS technicians ON sales_orders.distributor_shop_technician_id = technicians.id");
     }
 
     /**
