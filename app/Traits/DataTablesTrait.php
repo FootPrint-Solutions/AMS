9689ebe1e6b04ec5621159118712f8707b9f7a3f
William Oktavianus <williamoktavianus1099@gmail.com>
2024-03-27 15:05:34 +0700

    Adding default ordering in DataTables Trait

--- app/Traits/DataTablesTrait.php
+++ app/Traits/DataTablesTrait.php
@@ -11,9 +11,10 @@ trait DataTablesTrait
      * @param Illuminate\Database\Query\Builder $query Query built to obtain rows in table.
      * @param array $selectColumns The list of columns to be displayed.
      * @param array $searchColumns The list of columns to be searchable.
+     * @param array $searchColumns An array consists of two values : the default column order and the order direction (if null, table will be ordered by the update time in descending order by default).
      * @return array Associative array containing data for DataTables display.
      */
-    public static function getAllRows($request, $query, $selectColumns, $searchColumns = null)
+    public static function getAllRows($request, $query, $selectColumns, $searchColumns = null, $orderDefault = null)
     {
         // Get DataTables configuration request.
         $start = $request->input("start");
@@ -36,6 +37,12 @@ trait DataTablesTrait
             $columnName = $selectColumns[$orderColumn] ?? null;
             if ($columnName !== null) {
                 $query->orderBy($columnName, $orderDirection);
+            }
+        } else {
+            if ($orderDefault !== null) {
+                $query->orderBy($orderDefault["column"], $orderDefault["direction"]);
+            } else {
+                $query->orderBy("updated_at", "desc");
             }
         }
 
