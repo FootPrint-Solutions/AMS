93972273c566183dc5c21059a8a4f110fdfb3041
William Oktavianus <williamoktavianus1099@gmail.com>
2024-03-28 13:08:09 +0700

    Updating Sales Order Invoice

--- resources/views/Orders/SalesOrder/invoice.blade.php
+++ resources/views/Orders/SalesOrder/invoice.blade.php
@@ -4,139 +4,138 @@
     @media print {
         @page {
             size: A5 landscape;
-            margin: 0%;
+            font-size: 0.9em;
         }
     }
 
+    * {
+        font-size: 0.9em;
+    }
+
+    #table-detail thead {
+        border-bottom: double lightgray;
+    }
+
+    #table-detail-footer {
+        z-index: -10;
+        position: fixed;
+        bottom: 0;
+        left: 0;
+    }
+
     #invoice-company-logo {
-        width: 8em;
-        height: 8em;
+        width: 9.5em;
+        height: 9.5em;
+    }
+
+    .sticky-row {
+        flex-grow: 1;
+        /* Allow the last row to grow and push to the bottom */
     }
 </style>
 
-<div class="container">
-    <div class="head">
-        <div class="row">
-            <div class="col">
-                <div class="h1">INVOICE</div>
-                {{ $data['company']['name'] }}<br>
-                {{ $data['company']['address'] }}<br>
-                {{ $data['company']['contact'] }} | {{ $data['company']['email'] }}
-            </div>
+{{-- HEAD --}}
+<div id="invoice-head">
+    <table class="w-100">
+        <tr>
+            {{-- Invoice Information --}}
+            <td style="width: 50%">
+                <h3>INVOICE</h3>
+                {{ $data['profile']['sales_order_number'] }}<br>
+                Issued on {{ date('d M Y', strtotime($data['profile']['date'])) }}
+            </td>
 
-            <div class="col text-end">
-                <img src="/img/logos/256x256.png" id="invoice-company-logo">
-            </div>
-        </div>
-    </div>
-    <hr>
-
-    <table class="table">
-        <thead class="text-center">
-            <tr>
-                <th style="width: 33.3%">Customer</th>
-                <th style="width: 33.3%">Ship To</th>
-                <th style="width: 33.3%">Information</th>
-            </tr>
-        </thead>
-
-        <tbody>
-            {{-- DETAIL --}}
-            <tr>
-                <td>
-                    <div class="container">
-                        {{ $data['profile']['customer']['name'] }}<br>
-                        +62 {{ $data['profile']['customer']['contact'] }}<br>
-                        {{ $data['profile']['customer']['email'] }}<br>
-                        {{ $data['profile']['customer']['address'] }}<br>
+            {{-- Company Profile --}}
+            <td class="text-end" style="width: 50%">
+                <div class="row">
+                    <div class="col-9 text-end">
+                        <h3>{{ $data['company']['name'] }}</h3>
+                        {{ $data['company']['address'] }}<br>
+                        {{ $data['company']['contact'] }}<br>
+                        {{ $data['company']['email'] }}<br>
                     </div>
-                </td>
 
-                <td>
-                    <div class="container">
-                        {{ $data['profile']['customer']['name'] }}<br>
-                        +62 {{ $data['profile']['customer']['contact'] }}<br>
-                        {{ $data['profile']['customer']['email'] }}<br>
-                        {{ $data['profile']['address'] }}<br>
+                    <div class="col-3">
+                        <img src="/img/logos/256x256.png" id="invoice-company-logo">
                     </div>
-                </td>
-
-                <td>
-                    <div class="container">
-                        {{ $data['profile']['sales_order_number'] }} -
-                        {{ date('Y-m-d', strtotime($data['profile']['created_at'])) }}<br>
-                        {{ $data['profile']['shop']['name'] ?? '-' }}<br>
-                        {{ $data['profile']['customer']['contact'] }}<br>
-                    </div>
-                </td>
-            </tr>
-            </thead>
+                </div>
+            </td>
+        </tr>
     </table>
-    <br>
+</div>
+<hr class="my-1">
+
+{{-- BODY --}}
+<div id="invoice-body">
+    <table class="table table-sm">
+        <tr>
+            <td>
+                {{ $data['profile']['customer']['name'] }} (+62 {{ $data['profile']['customer']['contact'] }})<br>
+                {{ $data['profile']['customer']['email'] }}<br>
+                {{ $data['profile']['address'] }}
+            </td>
+        </tr>
+    </table>
 
     {{-- Detail Table --}}
-    <table class="table">
-        <thead class="text-center">
+    <table class="table table-sm" id="table-detail">
+        <thead>
             <tr>
-                <th style="width: 50%">Name</th>
+                <th style="width: 2%; font-size: 2em;">No</th>
+                <th style="width: 48%">Name</th>
                 <th style="width: 30%">Production Code</th>
-                <th style="width: 20%">Price (IDR)</th>
+                <th style="width: 20%; text-align: right;">Price (IDR)</th>
             </tr>
         </thead>
 
         <tbody>
             {{-- Detail --}}
-            @foreach ($data['profile']['batteries'] as $battery)
+            @foreach ($data['profile']['batteries'] as $index => $battery)
                 <tr>
+                    <td style="text-align: center">{{ $index + 1 }}</td>
                     <td>{{ $battery['battery_name'] }}</td>
                     <td>{{ $battery['battery_production_code'] }}</td>
                     <td style="text-align: right">{{ number_format($battery['battery_price']) }}</td>
                 </tr>
             @endforeach
         </tbody>
+        <hr class="m-0">
 
-        <tfoot>
-            <tr>
-                <td></td>
-                <th style="text-align: right">Subtotal</th>
-                <td style="text-align: right">
-                    {{ number_format($data['profile']['subtotal']) }}
-                </td>
-            </tr>
+        <table class="table table-sm" id="table-detail-footer">
+            <tfoot>
+                <tr>
+                    <td colspan="2" style="width: 50%"></td>
+                    <th style="text-align: right; width: 30%">Subtotal</th>
+                    <td style="text-align: right; width: 20%">
+                        {{ number_format($data['profile']['subtotal']) }}
+                    </td>
+                </tr>
 
-            <tr>
-                <td></td>
-                <th style="text-align: right">Tax</th>
-                <td style="text-align: right">
-                    {{ number_format(($data['profile']['subtotal'] * $data['profile']['tax']) / 100) }}
-                </td>
-            </tr>
+                <tr>
+                    <td colspan="2"></td>
+                    <th style="text-align: right">Discount</th>
+                    <td style="text-align: right">
+                        {{ number_format($data['profile']['discount_price']) }}
+                    </td>
+                </tr>
 
-            <tr>
-                <td></td>
-                <th style="text-align: right">Discount</th>
-                <td style="text-align: right">
-                    {{ number_format(($data['profile']['subtotal'] * $data['profile']['discount']) / 100) }}
-                </td>
-            </tr>
+                <tr>
+                    <td colspan="2"></td>
+                    <th style="text-align: right">Tax</th>
+                    <td style="text-align: right">
+                        {{ number_format($data['profile']['tax_price']) }}
+                    </td>
+                </tr>
 
-            <tr>
-                <td></td>
-                <th style="text-align: right">Extra Discount</th>
-                <td style="text-align: right">
-                    {{ number_format(($data['profile']['subtotal'] * $data['profile']['extra_discount']) / 100) }}
-                </td>
-            </tr>
-
-            <tr>
-                <td colspan="2"></td>
-                <td style="text-align: right">{{ number_format($data['profile']['total']) }}</td>
-            </tr>
-        </tfoot>
+                <tr>
+                    <td colspan="2"></td>
+                    <th style="text-align: right">Total</th>
+                    <td style="text-align: right">{{ number_format($data['profile']['total']) }}</td>
+                </tr>
+            </tfoot>
+        </table>
     </table>
 </div>
-
-
 
 <script>
     window.onload = function() {
