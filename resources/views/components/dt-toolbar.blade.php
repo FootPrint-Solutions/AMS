95f4450776db4053f7d80408bd278aa537cc7f3b
William Oktavianus <williamoktavianus1099@gmail.com>
2024-04-24 09:16:41 +0700

    Making dt-toolbar more global

--- resources/views/components/dt-toolbar.blade.php
+++ resources/views/components/dt-toolbar.blade.php
@@ -1,20 +1,25 @@
 <div class="btn-group">
     {{-- Edit --}}
-    <button class="btn btn-outline-primary btn-sm edit-selected" data-url={{ $editUrl }}
-        data-id={{ $idIdx }}>
-        <i class="fas fa-pencil"></i> Edit
-    </button>
+    @if (isset($editUrl) && $editUrl !== '' && $editUrl !== null)
+        <button class="btn btn-outline-primary btn-sm edit-selected" data-url={{ $editUrl }}
+            data-id={{ $idIdx }}>
+            <i class="fas fa-pencil"></i> Edit
+        </button>
+    @endif
 
-    {{-- Delete or Toggle --}}
+    {{-- Toggle --}}
+    @if (isset($toggleUrl) && ($toggleUrl !== '') & ($toggleUrl !== null))
+        <button class="btn btn-outline-info btn-sm toggle-selected ml-1" data-url={{ $toggleUrl }}
+            data-id={{ $idIdx }}>
+            <i class="fas fa-circle-dot"></i> Toggle Status
+        </button>
+    @endif
+
+    {{-- Delete --}}
     @if (isset($deleteUrl) && $deleteUrl !== '' && $deleteUrl !== null)
         <button class="btn btn-outline-danger btn-sm delete-selected ml-1" data-url={{ $deleteUrl }}
             data-id={{ $idIdx }}>
             <i class="fas fa-trash"></i> Delete
-        </button>
-    @else
-        <button class="btn btn-outline-info btn-sm toggle-selected ml-1" data-url={{ $toggleUrl }}
-            data-id={{ $idIdx }}>
-            <i class="fas fa-circle-dot"></i> Toggle Status
         </button>
     @endif
 </div>
