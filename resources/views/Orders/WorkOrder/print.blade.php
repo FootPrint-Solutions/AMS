e1fe229a5aff70c35547c8cfced7dcb9d22b48ee
R4D3N G0Z4LL <radengozal@gmail.com>
2024-06-04 16:06:31 +0700

    Refactor SalesOrderModel and QuickQuotation index.blade.php

--- resources/views/Orders/WorkOrder/print.blade.php
+++ resources/views/Orders/WorkOrder/print.blade.php
@@ -6,9 +6,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Work Order Print</title>
     <style>
+        hr.dashed {
+            border: none;
+            border-top: 2px dashed #000;
+            color: #000;
+            background-color: #fff;
+            height: 1px;
+        }
+
         body {
             font-family: Arial, sans-serif;
-            margin: 10px;
+            /* margin: 10px; */
             font-size: 12px;
         }
 
@@ -59,109 +67,258 @@
 
         .section th,
         .section td {
-            padding: 5px;
+            padding: 4px;
             text-align: left;
         }
 
-        .section .details td {
+        * {
+            box-sizing: border-box;
+        }
+
+        /* Create two equal columns that floats next to each other */
+        .column {
+            float: left;
             width: 50%;
+
+        }
+
+        /* Clear floats after the columns */
+        .row:after {
+            content: "";
+            display: table;
+            clear: both;
         }
     </style>
 </head>
 
 <body>
-    <div class="container">
-        <div class="header">
-            <h1>Work Order</h1>
-            <h2>Work Order Number: {{ $workOrder['work_order_number'] }}</h2>
-        </div>
 
-        <div class="section">
-            <h3>Work Order Master</h3>
-            <table>
-                <tr class="details">
-                    <td>Date</td>
-                    <td>{{ $workOrder['date'] }}</td>
-                </tr>
-                <tr class="details">
-                    <td>Sales Order ID</td>
-                    <td>{{ $workOrder['sales_order_id'] }}</td>
-                </tr>
-                <tr class="details">
-                    <td>Customer ID</td>
-                    <td>{{ $workOrder['customer_id'] }}</td>
-                </tr>
-                <tr class="details">
-                    <td>Tax</td>
-                    <td>{{ $workOrder['tax'] }}</td>
-                </tr>
-                <tr class="details">
-                    <td>Tax Price</td>
-                    <td>{{ $workOrder['tax_price'] }}</td>
-                </tr>
-                <tr class="details">
-                    <td>Discount</td>
-                    <td>{{ $workOrder['discount'] }}</td>
-                </tr>
-                <tr class="details">
-                    <td>Discount Price</td>
-                    <td>{{ $workOrder['discount_price'] }}</td>
-                </tr>
-                <tr class="details">
-                    <td>Total</td>
-                    <td>{{ $workOrder['total'] }}</td>
-                </tr>
-                <tr class="details">
-                    <td>Address</td>
-                    <td>{{ $workOrder['address'] }}</td>
-                </tr>
-                <tr class="details">
-                    <td>Latitude</td>
-                    <td>{{ $workOrder['latitude'] }}</td>
-                </tr>
-                <tr class="details">
-                    <td>Longitude</td>
-                    <td>{{ $workOrder['longitude'] }}</td>
-                </tr>
-            </table>
+    <div class="section">
+        <div style='text-align:end;margin-bottom:-15px;'>
+            <h1>Instruksi Kerja Teknisi</h1>
         </div>
+        <table>
+            <tr>
+                <td style='width:60%;'>Nama Partner : </td>
+                <td>Order ID :</td>
+            </tr>
+            <tr>
+                <td style='width:60%;'>Admin Akikita :</td>
+                <td>Tanggal : </td>
+            </tr>
+            <tr>
+                <td style='width:60%;'>Jenis Pesanan : </td>
+                <td>Waktu Pesanan:</td>
+            </tr>
+        </table>
 
-        <div class="section">
-            <h3>Work Order Battery (Detail)</h3>
-            <table>
-                <tr>
-                    <th>ID</th>
-                    <th>Work Order ID</th>
-                    <th>Battery ID</th>
-                    <th>Battery Name</th>
-                    <th>Battery Price</th>
-                    <th>Quantity</th>
-                    <th>Created At</th>
-                </tr>
-                <!-- Example row, this should be generated dynamically -->
-                <?php 
-                $id = 1;
-                foreach ($workOrder['batteries'] as $battery) {
-                    $work_order_id = $battery['work_order_id'];
-                    $battery_id = $battery['battery_id'];
-                    $battery_name = $battery['battery_name'];
-                    $battery_price = $battery['battery_price'];
-                    $quantity = $battery['quantity'];
-                    $created_at = $battery['created_at'];
-                ?>
-                <tr>
-                    <td>{{ $id }}</td>
-                    <td>{{ $work_order_id }}</td>
-                    <td>{{ $battery_id }}</td>
-                    <td>{{ $battery_name }}</td>
-                    <td>{{ $battery_price }}</td>
-                    <td>{{ $quantity }}</td>
-                    <td>{{ $created_at }}</td>
-                </tr>
-                <?php $id++; } ?>
-                <!-- Repeat for each battery detail -->
-            </table>
+        <table style='margin-top:10px;'>
+            <tr>
+                <td colspan="3" style="text-align:center; font-weight:700;">Informasi Pesanan</td>
+
+            </tr>
+            <tr>
+                <td style='width:60%;' rowspan="2">Alamat : </td>
+                <td style='width:20%;'>Nama Pelanggan :</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td style='width:20%;'>Kendaraan :</td>
+            </tr>
+        </table>
+        <table style='margin-top:10px;'>
+            <tr>
+                <td colspan="4" style="text-align:center; font-weight:700;">Pekerjaan</td>
+            </tr>
+            <tr>
+                <td style='width:1%; vertical-align:top;' rowspan="5">1. </td>
+                <td style='width:40%;'>Siapkan :</td>
+                <td style='width:10%;'>Jumlah</td>
+                <td rowspan="5" style="vertical-align:top;">Syarat Kondisi Aki:</td>
+            </tr>
+            <tr>
+                <td style='width:20%;'><input type='checkbox'></td>
+                <td></td>
+
+            </tr>
+            <tr>
+                <td style='width:20%;'><input type='checkbox'></td>
+                <td></td>
+
+            </tr>
+            <tr>
+                <td style='width:20%;'><input type='checkbox'></td>
+                <td></td>
+
+            </tr>
+            <tr>
+                <td style='width:20%; text-align:end;'>Total</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td style='width:1%;'>2. </td>
+                <td colspan="4"><input type='checkbox'></td>
+            </tr>
+            <tr>
+                <td style='width:1%;'>3. </td>
+                <td colspan="4"><input type='checkbox'></td>
+            </tr>
+            <tr>
+                <td style='width:1%;'>4. </td>
+                <td colspan="4"><input type='checkbox'></td>
+            </tr>
+            <tr>
+                <td style='width:1%;'>5. </td>
+                <td colspan="4"><input type='checkbox'></td>
+            </tr>
+            <tr>
+                <td style='width:1%;'>6. </td>
+                <td colspan="4"><input type='checkbox'></td>
+            </tr>
+            <tr>
+                <td style='width:1%;'>7. </td>
+                <td colspan="4"><input type='checkbox'></td>
+            </tr>
+            <tr>
+                <td style='width:1%;'>8. </td>
+                <td colspan="4"><input type='checkbox'></td>
+            </tr>
+            <tr>
+                <td style='width:1%;'>9. </td>
+                <td colspan="4"><input type='checkbox'></td>
+            </tr>
+            <tr>
+                <td style='width:1%;'>10. </td>
+                <td colspan="4"><input type='checkbox'></td>
+            </tr>
+            <tr>
+                <td style='width:1%;'>11. </td>
+                <td colspan="4"><input type='checkbox'></td>
+            </tr>
+
+        </table>
+
+        <table style='margin-top:10px;'>
+            <tr>
+                <td colspan="3" style="">Catatan</td>
+
+            </tr>
+            <tr>
+                <td style='width:60%; padding:50px' rowspan="3" colspan="3"></td>
+            </tr>
+        </table>
+
+        <div class="row">
+            <div class="column">
+                <table style='margin-top:10px;'>
+                    <tr>
+                        <td colspan="2" style="text-align:center; font-weight:700;">Tanda Tangan</td>
+                    </tr>
+                    <tr>
+                        <td style="text-align:center; width:50%;">Admin Partner</td>
+                        <td style="text-align:center;">Teknisi</td>
+                    </tr>
+                    <tr>
+                        <td style="padding:40px; width:50%;"></td>
+                        <td style="padding:40px"></td>
+                    </tr>
+                </table>
+            </div>
+            <div class="column">
+                <table style='margin-top:10px;'>
+                    <tr>
+                        <td colspan="4" style="text-align:center; font-weight:700;">QR Code Lokasi</td>
+                    </tr>
+                    <tr>
+                        <td style="padding:53px"></td>
+                    </tr>
+                </table>
+            </div>
         </div>
+    </div>
+    <br><br>
+    <div class="section">
+        <div style='text-align:end;margin-bottom:-15px;'>
+            <h1>Instruksi Kerja Admin Partner</h1>
+        </div>
+        <table>
+            <tr>
+                <td style='width:60%;'>Nama Partner : </td>
+                <td>Order ID :</td>
+            </tr>
+            <tr>
+                <td style='width:60%;'>Admin Akikita :</td>
+                <td>Tanggal : </td>
+            </tr>
+            <tr>
+                <td style='width:60%;'>Jenis Pesanan : </td>
+                <td>Waktu Pesanan:</td>
+            </tr>
+        </table>
+        <div class="row">
+            <div class="column" style='padding: 10px;'>
+                <table style='margin-top:10px;'>
+                    <tr>
+                        <td colspan="2" style="text-align:center; font-weight:700;">Pekerjaan</td>
+                    </tr>
+                    <tr>
+                        <td style='width:10%;'></td>
+                        <td><input type='checkbox'></td>
+                    </tr>
+                    <tr>
+                        <td style='width:10%;'></td>
+                        <td><input type='checkbox'></td>
+                    </tr>
+                    <tr>
+                        <td style='width:10%;'></td>
+                        <td><input type='checkbox'></td>
+                    </tr>
+                    <tr>
+                        <td style='width:10%;'></td>
+                        <td><input type='checkbox'></td>
+                    </tr>
+                    <tr>
+                        <td style='width:10%;'></td>
+                        <td><input type='checkbox'></td>
+                    </tr>
+                    <tr>
+                        <td style='width:10%;'></td>
+                        <td><input type='checkbox'></td>
+                    </tr>
+                    <tr>
+                        <td style='width:10%;'></td>
+                        <td><input type='checkbox'></td>
+                    </tr>
+                    <tr>
+                        <td style='width:10%;'></td>
+                        <td><input type='checkbox'></td>
+                    </tr>
+                    <tr>
+                        <td style='width:10%;'></td>
+                        <td><input type='checkbox'></td>
+                    </tr>
+                    <tr>
+                        <td style='width:10%;'></td>
+                        <td><input type='checkbox'></td>
+                    </tr>
+                </table>
+            </div>
+            <div class="column" style='padding: 10px;'>
+                <table style='margin-top:10px;'>
+                    <tr>
+                        <td colspan="4" style="text-align:center; font-weight:700;">Tanda Tangan Admin Parnter</td>
+                    </tr>
+                    <tr>
+                        <td colspan="2" style="padding:53px"></td>
+                    </tr>
+                    <tr>
+                        <td width="20%">Nama : </td>
+                        <td></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+        <hr class="dashed">
     </div>
 </body>
 
