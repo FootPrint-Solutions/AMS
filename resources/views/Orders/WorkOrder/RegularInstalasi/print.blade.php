3e48db4c3aa38ef16d821ece2a1d60402c920d13
R4D3N G0Z4LL <radengozal@gmail.com>
2024-06-21 11:27:08 +0700

    chore: Update QR code size for work order print view

--- resources/views/Orders/WorkOrder/RegularInstalasi/print.blade.php
+++ resources/views/Orders/WorkOrder/RegularInstalasi/print.blade.php
@@ -10,23 +10,27 @@
             border: none;
             border-top: 2px dashed #000;
             color: #000;
-            background-color: #fff;
+            background-color: #f4f4f4;
             height: 1px;
         }
 
         body {
             font-family: Arial, sans-serif;
-            /* margin: 10px; */
             font-size: 12px;
+            background-color: #f4f4f4;
         }
 
         .container {
             width: 100%;
-            max-width: 800px;
+            max-width: 148mm;
+            /* Set max-width to A5 width */
+            height: 210mm;
+            /* Set height to A5 height */
             margin: 0 auto;
             padding: 10px;
             border: 1px solid #000;
             box-sizing: border-box;
+            background-color: #fff;
         }
 
         .header {
@@ -67,7 +71,7 @@
 
         .section th,
         .section td {
-            padding: 4px;
+            /* padding: 4px; */
             text-align: left;
         }
 
@@ -75,153 +79,167 @@
             box-sizing: border-box;
         }
 
-        /* Create two equal columns that floats next to each other */
         .column {
             float: left;
             width: 50%;
-
         }
 
-        /* Clear floats after the columns */
         .row:after {
             content: "";
             display: table;
             clear: both;
+        }
+
+        @media print {
+            @page {
+                size: A5;
+                margin: 0;
+            }
+
+            body {
+                margin: 0;
+            }
+
+            .container {
+                border: none;
+                width: 100%;
+                height: 100%;
+            }
         }
     </style>
 </head>
 
 <body>
 
-    <div class="section">
-        <div style='display:flex; justify-content:space-between; align-items:center; margin-bottom:-15px;'>
-            <h1>{{ $workOrder->work_order_number }}</h1>
-            <h1>Instruksi Kerja Teknisi</h1>
-        </div>
-        <table>
-            <tr>
-                <td style='width:60%;'>Nama Partner : {{ $workOrder->salesOrder->distributorShop->name ?? '' }}</td>
-                <td>Order ID : {{ $workOrder->salesOrder->sales_order_number }}</td>
-            </tr>
-            <tr>
-                <td style='width:60%;'>Admin Akikita : {{ auth()->user()->name }}</td>
-                <td>Tanggal : {{ date('d-m-Y', strtotime($workOrder->salesOrder->date)) }}</td>
-            </tr>
-            <tr>
-                <td style='width:60%;'>Jenis Pesanan : Regular dan instalasi</td>
-                <td>Waktu Pesanan: {{ date('d-m-Y H:i:s', strtotime($workOrder->salesOrder->created_at)) }}</td>
-            </tr>
-        </table>
-
-        <table style='margin-top:10px;'>
-            <tr>
-                <td colspan="3" style="text-align:center; font-weight:700;">Informasi Pesanan</td>
-
-            </tr>
-            <tr>
-                <td style='width:60%;' rowspan="2">Alamat : {{ $workOrder->address }}</td>
-                <td style='width:20%;'>Nama Pelanggan :</td>
-                <td>{{ $workOrder->customer->name }}</td>
-            </tr>
-            <tr>
-                <td style='width:20%;'>Kendaraan : </td>
-                <td>{{ $workOrder->salesOrder->vehicle->name ?? '' }}</td>
-            </tr>
-        </table>
-        <table style='margin-top:10px;'>
-            <tr>
-                <td colspan="4" style="text-align:center; font-weight:700;">Pekerjaan</td>
-            </tr>
-            <tr>
-                <td style='width:1%; vertical-align:top;' rowspan="5">1. </td>
-                <td style='width:40%;'>Siapkan :</td>
-                <td style='width:10%;'>Jumlah</td>
-                <td rowspan="5" style="vertical-align:top;">Syarat Kondisi Aki:</td>
-            </tr>
-            @php $no = 1; @endphp
-            @php $count = count($workOrder->batteries); @endphp
-
-            @foreach ($workOrder->batteries as $battery)
-                <tr>
-                    <td><input type='checkbox'>{{ $battery->battery_name }}</td>
-                    <td>{{ $battery->quantity }}</td>
-                </tr>
-                @php $no++; @endphp
-            @endforeach
-
-            @while ($no <= 3)
-                <tr>
-                    <td><input type='checkbox'>{{ '' }}</td>
-                    <td>{{ '' }}</td>
-                </tr>
-                @php $no++; @endphp
-            @endwhile
-
-            <tr>
-                <td style='width:20%; text-align:end;'>Total</td>
-                <td>{{ $count }}</td>
-            </tr>
-
-            @for ($i = 0; $i < 10; $i++)
-                @if ($i < count($taskOne))
-                    <tr>
-                        <td style='width:1%;'>{{ $i + 1 }}. </td>
-                        <td colspan="4"><input type='checkbox'>{{ $taskOne[$i]->message }}</td>
-                    </tr>
-                @else
-                    <tr>
-                        <td style='width:1%;'>{{ $i + 1 }}. </td>
-                        <td colspan="4"><input type='checkbox'></td> <!-- Jika tidak ada data, kolom akan kosong -->
-                    </tr>
-                @endif
-            @endfor
-        </table>
-
-        <table style='margin-top:10px;'>
-            <tr>
-                <td colspan="3">Catatan</td>
-
-            </tr>
-            <tr>
-                <td style='width:60%; padding:50px' rowspan="3" colspan="3"></td>
-            </tr>
-        </table>
-
-        <div class="row">
-            <div class="column">
-                <table style='margin-top:10px;'>
-                    <tr>
-                        <td colspan="2" style="text-align:center; font-weight:700;">Tanda Tangan</td>
-                    </tr>
-                    <tr>
-                        <td style="text-align:center; width:50%;">Admin Partner</td>
-                        <td style="text-align:center;">Teknisi</td>
-                    </tr>
-                    <tr>
-                        <td style="padding:40px; width:50%;"></td>
-                        <td style="padding:40px"></td>
-                    </tr>
-                </table>
+    <div class="container">
+        <div class="section">
+            <div style='display:flex; justify-content:space-between; align-items:center; margin-bottom:-15px;'>
+                <h2>{{ $workOrder->work_order_number }}</h2>
+                <h2>Instruksi Kerja Teknisi</h2>
             </div>
-            <div class="column">
-                <table style='margin-top:10px;'>
+            <table>
+                <tr>
+                    <td style='width:60%;'>Nama Partner : {{ $workOrder->salesOrder->distributorShop->name ?? '' }}</td>
+                    <td>Order ID : {{ $workOrder->salesOrder->sales_order_number }}</td>
+                </tr>
+                <tr>
+                    <td style='width:60%;'>Admin Akikita : {{ auth()->user()->name }}</td>
+                    <td>Tanggal : {{ date('d-m-Y', strtotime($workOrder->salesOrder->date)) }}</td>
+                </tr>
+                <tr>
+                    <td style='width:60%;'>Jenis Pesanan : Regular dan instalasi</td>
+                    <td>Waktu Pesanan: {{ date('d-m-Y H:i:s', strtotime($workOrder->salesOrder->created_at)) }}</td>
+                </tr>
+            </table>
+
+            <table style='margin-top:10px;'>
+                <tr>
+                    <td colspan="3" style="text-align:center; font-weight:700;">Informasi Pesanan</td>
+
+                </tr>
+                <tr>
+                    <td style='width:60%;' rowspan="2">Alamat : {{ $workOrder->address }}</td>
+                    <td style='width:20%;'>Nama Pelanggan :</td>
+                    <td>{{ $workOrder->customer->name }}</td>
+                </tr>
+                <tr>
+                    <td style='width:20%;'>Kendaraan : </td>
+                    <td>{{ $workOrder->salesOrder->vehicle->name ?? '' }}</td>
+                </tr>
+            </table>
+            <table style='margin-top:10px;'>
+                <tr>
+                    <td colspan="4" style="text-align:center; font-weight:700;">Pekerjaan</td>
+                </tr>
+                <tr>
+                    <td style='width:1%; vertical-align:top;' rowspan="5">1. </td>
+                    <td style='width:40%;'>Siapkan :</td>
+                    <td style='width:10%;'>Jumlah</td>
+                    <td rowspan="5" style="vertical-align:top;">Syarat Kondisi Aki:</td>
+                </tr>
+                @php $no = 1; @endphp
+                @php $count = count($workOrder->batteries); @endphp
+
+                @foreach ($workOrder->batteries as $battery)
                     <tr>
-                        <td colspan="4" style="text-align:center; font-weight:700;">QR Code Lokasi</td>
+                        <td><input type='checkbox'>{{ $battery->battery_name }}</td>
+                        <td>{{ $battery->quantity }}</td>
                     </tr>
+                    @php $no++; @endphp
+                @endforeach
+
+                @while ($no <= 3)
                     <tr>
-                        <td style="text-align:center;">{{ $qrCode }}</td>
+                        <td><input type='checkbox'>{{ '' }}</td>
+                        <td>{{ '' }}</td>
                     </tr>
-                </table>
+                    @php $no++; @endphp
+                @endwhile
+
+                <tr>
+                    <td style='width:20%; text-align:end;'>Total</td>
+                    <td>{{ $count }}</td>
+                </tr>
+
+                @for ($i = 0; $i < 10; $i++)
+                    @if ($i < count($taskOne))
+                        <tr>
+                            <td style='width:1%;'>{{ $i + 1 }}. </td>
+                            <td colspan="4"><input type='checkbox'>{{ $taskOne[$i]->message }}</td>
+                        </tr>
+                    @else
+                        <tr>
+                            <td style='width:1%;'>{{ $i + 1 }}. </td>
+                            <td colspan="4"><input type='checkbox'></td>
+                            <!-- Jika tidak ada data, kolom akan kosong -->
+                        </tr>
+                    @endif
+                @endfor
+            </table>
+
+            <table style='margin-top:10px;'>
+                <tr>
+                    <td colspan="3">Catatan</td>
+
+                </tr>
+                <tr>
+                    <td style='width:60%; padding:50px' rowspan="3" colspan="3"></td>
+                </tr>
+            </table>
+
+            <div class="row">
+                <div class="column">
+                    <table style='margin-top:10px;'>
+                        <tr>
+                            <td colspan="2" style="text-align:center; font-weight:700;">Tanda Tangan</td>
+                        </tr>
+                        <tr>
+                            <td style="text-align:center; width:50%;">Admin Partner</td>
+                            <td style="text-align:center;">Teknisi</td>
+                        </tr>
+                        <tr>
+                            <td style="padding:30px; width:50%;"></td>
+                            <td style="padding:30px"></td>
+                        </tr>
+                    </table>
+                </div>
+                <div class="column">
+                    <table style='margin-top:10px;'>
+                        <tr>
+                            <td colspan="4" style="text-align:center; font-weight:700;">QR Code Lokasi</td>
+                        </tr>
+                        <tr>
+                            <td style="text-align:center;">{{ $qrCode }}</td>
+                        </tr>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
-    <br><br>
 
+    <script>
+        window.print();
+    </script>
 </body>
 
 </html>
-
-{{-- js print --}}
-<script>
-    window.print();
-</script>
--- resources/views/Orders/WorkOrder/Technician/print.blade.php
+++ resources/views/Orders/WorkOrder/Technician/print.blade.php
@@ -5,139 +5,192 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laporan Teknisi</title>
+    <style>
+        hr.dashed {
+            border: none;
+            border-top: 2px dashed #000;
+            color: #000;
+            background-color: #f4f4f4;
+            height: 1px;
+        }
+
+        body {
+            font-family: Arial, sans-serif;
+            font-size: 12px;
+            background-color: #f4f4f4;
+        }
+
+        .container {
+            width: 100%;
+            max-width: 148mm;
+            /* Set max-width to A5 width */
+            height: 210mm;
+            /* Set height to A5 height */
+            margin: 0 auto;
+            padding: 10px;
+            border: 1px solid #000;
+            box-sizing: border-box;
+            background-color: #fff;
+        }
+
+        .header {
+            text-align: center;
+            margin-bottom: 10px;
+        }
+
+        .header h1 {
+            margin: 0;
+            font-size: 18px;
+        }
+
+        .header h2 {
+            margin: 0;
+            font-size: 14px;
+            font-weight: normal;
+        }
+
+        .section {
+            margin-bottom: 10px;
+        }
+
+        .section h3 {
+            margin-bottom: 5px;
+            font-size: 14px;
+        }
+
+        .section table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+
+        .section table,
+        .section th,
+        .section td {
+            border: 1px solid #000;
+        }
+
+        .section th,
+        .section td {
+            /* padding: 4px; */
+            text-align: left;
+        }
+
+        * {
+            box-sizing: border-box;
+        }
+
+        .column {
+            float: left;
+            width: 50%;
+        }
+
+        .row:after {
+            content: "";
+            display: table;
+            clear: both;
+        }
+
+        @media print {
+            @page {
+                size: A5;
+                margin: 0;
+            }
+
+            body {
+                margin: 0;
+            }
+
+            .container {
+                border: none;
+                width: 100%;
+                height: 100%;
+            }
+        }
+
+        .signatures {
+            margin-bottom: 20px;
+        }
+
+        .signature-line {
+            border-bottom: 1px solid #000;
+            margin-top: 20px;
+            margin-bottom: 5px;
+            width: 50%;
+        }
+
+        .signatures {
+            display: flex;
+            justify-content: space-between;
+        }
+
+        .customer-signature {
+            width: 45%;
+            text-align: center;
+        }
+    </style>
 </head>
-<style>
-    body {
-        font-family: Arial, sans-serif;
-        margin: 0;
-        padding: 0;
-        background-color: #f4f4f4;
-    }
-
-    .report-container {
-        width: 80%;
-        margin: 20px auto;
-        padding: 20px;
-        background-color: #fff;
-        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-    }
-
-    h1 {
-        text-align: center;
-        color: #333;
-    }
-
-    .order-details,
-    .address,
-    .battery,
-    .vehicle-condition,
-    .battery-tension,
-    .current-while-off,
-    .notes,
-    .signatures {
-        margin-bottom: 20px;
-    }
-
-    strong {
-        color: #333;
-    }
-
-    .input-field {
-        width: 100px;
-        padding: 5px;
-        margin-left: 10px;
-    }
-
-    .notes-details {
-        width: 100%;
-        height: 100px;
-        padding: 10px;
-    }
-
-    .signature-line {
-        border-bottom: 1px solid #000;
-        margin-top: 20px;
-        margin-bottom: 5px;
-        width: 50%;
-    }
-
-    .signatures {
-        display: flex;
-        justify-content: space-between;
-    }
-
-    .technician-signature,
-    .customer-signature {
-        width: 45%;
-        text-align: center;
-    }
-
-    table {
-        width: 100%;
-        border-collapse: collapse;
-    }
-
-    table,
-    th,
-    td {
-        border: 1px solid #000;
-    }
-</style>
 
 <body>
-    <div class="report-containezr">
-        <h1>Laporan Teknisi Akikita.id</h1>
-        <table>
-            <tr>
-                <td>
-                    <strong>Order ID: </strong>{{ $workOrder->work_order_number }}
-                </td>
-                <td>
-                    <strong>Tanggal: </strong> {{ date('d-m-Y', strtotime($workOrder->salesOrder->date)) }}
-                </td>
-            </tr>
-            <tr>
-                <td colspan="2"> <strong>Alamat: </strong>{{ $workOrder->address }}</td>
-            </tr>
-            <tr>
-                <td colspan="2"> <strong>Aki:</strong>
-                    @foreach ($workOrder->batteries as $battery)
-                        {{ $battery->battery_name }},
-                    @endforeach
-                </td>
-            </tr>
-        </table>
-        <div class="vehicle-condition">
-            <p><strong>Kondisi Kendaraan:</strong></p>
-            <p>Apakah kendaraan dapat dinyalakan setelah pemasangan aki?</p>
-            <label><input type="radio" name="start-status" value="ya"> Ya</label>
-            <label><input type="radio" name="start-status" value="tidak"> Tidak</label>
-        </div>
-        <div class="battery-tension">
-            <p><strong>Ketegangan mekanisme pengisian daya aki:</strong></p>
-            __________________Volt <span style="margin-left: 80px;">(Optimal: 14V – 14.5V)</span>
-        </div>
-        <div class="current-while-off">
-            <p><strong>Arus listrik saat kendaraan mati:</strong></p>
-            __________________Amp <span style="margin-left: 80px;">(Optimal: 0.10A – 0.50A)</span>
-        </div>
-        <table style='margin-top:10px;'>
-            <p><strong>Keterangan:</strong></p>
-            <tr>
-                <td style='width:60%; padding:50px' rowspan="3" colspan="3"></td>
-            </tr>
-        </table>
-        <div class="signatures">
-            <div class="technician-signature">
-                <p><strong>Tanda Tangan Teknisi</strong></p>
-                <p style="margin-top: 45px;"><strong>Nama:</strong> <span class="technician-name"></span></p>
+
+    <div class="container">
+        <div class="section">
+            <h2>Laporan Teknisi Akikita.id</h2>
+            <table>
+                <tr>
+                    <td>
+                        <strong>Order ID: </strong>{{ $workOrder->work_order_number }}
+                    </td>
+                    <td>
+                        <strong>Tanggal: </strong> {{ date('d-m-Y', strtotime($workOrder->salesOrder->date)) }}
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2"> <strong>Alamat: </strong>{{ $workOrder->address }}</td>
+                </tr>
+                <tr>
+                    <td colspan="2"> <strong>Aki:</strong>
+                        @foreach ($workOrder->batteries as $battery)
+                            {{ $battery->battery_name }},
+                        @endforeach
+                    </td>
+                </tr>
+            </table>
+            <div class="vehicle-condition">
+                <p><strong>Kondisi Kendaraan:</strong></p>
+                <p>Apakah kendaraan dapat dinyalakan setelah pemasangan aki?</p>
+                <label><input type="radio" name="start-status" value="ya"> Ya</label>
+                <label><input type="radio" name="start-status" value="tidak"> Tidak</label>
             </div>
-            <div class="customer-signature">
-                <p><strong>Tanda Tangan Pelanggan</strong></p>
-                <p style="margin-top: 45px;"><strong>Nama:</strong> <span class="customer-name"></span></p>
+            <div class="battery-tension">
+                <p><strong>Ketegangan mekanisme pengisian daya aki:</strong></p>
+                __________________Volt <span style="margin-left: 80px;">(Optimal: 14V – 14.5V)</span>
+            </div>
+            <div class="current-while-off">
+                <p><strong>Arus listrik saat kendaraan mati:</strong></p>
+                __________________Amp <span style="margin-left: 80px;">(Optimal: 0.10A – 0.50A)</span>
+            </div>
+            <table style='margin-top:10px;'>
+                <p><strong>Keterangan:</strong></p>
+                <tr>
+                    <td style='width:60%; padding:50px' rowspan="3" colspan="3"></td>
+                </tr>
+            </table>
+            <div class="signatures">
+                <div class="technician-signature">
+                    <p><strong>Tanda Tangan Teknisi</strong></p>
+                    <p style="margin-top: 45px;"><strong>Nama:</strong> <span class="technician-name"></span></p>
+                </div>
+                <div class="customer-signature">
+                    <p><strong>Tanda Tangan Pelanggan</strong></p>
+                    <p style="margin-top: 45px;"><strong>Nama:</strong> <span class="customer-name"></span></p>
+                </div>
             </div>
         </div>
     </div>
+
+    <script>
+        window.print();
+    </script>
 </body>
 
 </html>
