fec32b07a518408100460b8e7c35f685d2a24a8e
R4D3N G0Z4LL <radengozal@gmail.com>
2024-06-25 15:03:20 +0700

    chore: Update button label to "Convert to Image" in QuickQuotation step-1 view

--- resources/views/Orders/QuickQuotation/step-1.blade.php
+++ resources/views/Orders/QuickQuotation/step-1.blade.php
@@ -62,7 +62,7 @@
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="ModalScreenshotLabel">
-                    <button id="screenshoot-btn" class="btn btn-primary">Save to Image</button>
+                    <button id="screenshoot-btn" class="btn btn-primary">Convert to Image</button>
                 </h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
