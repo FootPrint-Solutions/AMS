64e494cce2715e5e971c0b50c181a00a5a0cea97
R4D3N G0Z4LL <radengozal@gmail.com>
2024-08-02 17:00:16 +0700

    Refactor: Add detail WO + Add button function

--- resources/views/Mobile/Orders/WorkOrder/index.blade.php
+++ resources/views/Mobile/Orders/WorkOrder/index.blade.php
@@ -18,7 +18,7 @@
             </div>
             <div class="col text-justify">
                 {{-- button rounded with dot icon --}}
-                <button class="btn btn-secondary float-right btn-sm btn-rounded text-center mt-2">
+                <button class="btn btn-secondary float-right btn-sm btn-rounded text-center mt-2" id="btn-pop-up">
                     <i class="material-icons">more_horiz</i>
                 </button>
             </div>
@@ -36,6 +36,144 @@
         </div>
     </div>
 </div>
+
+{{-- Modal Work Order Mobile Menu --}}
+<div class="modal fade" id="modal-work-order-mobile-menu" tabindex="-1" role="dialog"
+    aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-body text-center">
+                <div class="container mt-5">
+                    <div class="row">
+                        <div class="col-6">
+                            <button class="custom-btn delete-btn">
+                                <i class="fas fa-times-circle"></i>
+                                Delete Work Order
+                            </button>
+                        </div>
+                        <div class="col-6">
+                            <button class="custom-btn print-work-btn">
+                                <i class="fas fa-print"></i>
+                                Print Work <br> Order
+                            </button>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-6">
+                            <button class="custom-btn print-tech-btn">
+                                <i class="fas fa-file-alt"></i>
+                                Print Technician Report
+                            </button>
+                        </div>
+                        <div class="col-6">
+                            <button class="custom-btn complete-btn">
+                                <i class="fas fa-check-circle"></i>
+                                Complete Work Order
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+{{-- Modal Work Order Detail --}}
+<div class="modal fade" id="modal-work-order-detail" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="detailWOModalLabel">Detail WO</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+
+            <div class="modal-body">
+                <table>
+                    <tr>
+                        <td width="50%%">WO Number</td>
+                        <td width="50%">:</td>
+                        <td width="50%">WO240700008</td>
+                    </tr>
+                    <tr>
+                        <td>SO Number</td>
+                        <td>:</td>
+                        <td>AK240700008</td>
+                    </tr>
+                    <tr>
+                        <td>Date</td>
+                        <td>:</td>
+                        <td>3 Juli 2024</td>
+                    </tr>
+                    <tr>
+                        <td>Customer</td>
+                        <td>:</td>
+                        <td>Ginanjar</td>
+                    </tr>
+                    <tr>
+                        <td>Address</td>
+                        <td>:</td>
+                        <td>Padasuka</td>
+                    </tr>
+                    <tr>
+                        <td>Discount</td>
+                        <td>:</td>
+                        <td>8.05%</td>
+                    </tr>
+                    <tr>
+                        <td>Extra Discount</td>
+                        <td>:</td>
+                        <td>0.00%</td>
+                    </tr>
+                    <tr>
+                        <td>Total (IDR)</td>
+                        <td>:</td>
+                        <td>800.000</td>
+                    </tr>
+                    <tr>
+                        <td>Status</td>
+                        <td>:</td>
+                        <td>Draft</td>
+                    </tr>
+                </table>
+                <br>
+                <div class="detail-section">
+                    <div class="section-title">Batteries</div>
+                    <div class="badge-custom">
+                        <span class="text-battery-detail">AMARON Hi-Life 42B20L</span>
+                        <br>
+                        <table>
+                            <tr>
+                                <td width="60%">800.000</td>
+                                <td width="50%">1</td>
+                                <td width="50%">800.000</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+                <table>
+                    <tr>
+                        <td width="50%">Payment Status</td>
+                        <td width="50%">:</td>
+                        <td width="50%">paid</td>
+                    </tr>
+                    <tr>
+                        <td>Payment Method</td>
+                        <td>:</td>
+                        <td>Midtrans</td>
+                    </tr>
+                    <tr>
+                        <td>Payment Link</td>
+                        <td>:</td>
+                        <td><a href="#">Payment Link</a></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+
+<input type="hidden" name="work_order_id" id="work_order_id">
 
 <script>
     var isLoading = false; // Flag to check if lazy loading is in progress
@@ -59,17 +197,52 @@
             }
         });
 
-        // when click work order list
+        // when btn-work-order-card clicked add class active
+        $(document).on('click', '#btn-work-order-card', function() {
+            $('.bg-dark-blue').removeClass('btn-wo-active');
+            $(this).closest('#btn-work-order-card').addClass('btn-wo-active');
+        });
+
         $('#lazy-load-list-data').on('click', '#work-order-list', function() {
             var workOrderId = $(this).data('id');
-            // add class active to selected work order
-            $('#lazy-load-list-data').find('.card').removeClass('card-active-work-order');
-            $(this).addClass('card-active-work-order');
+
+            // change button color 
+            $('.bg-dark-blue').removeClass('btn-wo-active');
+            $(this).find('.bg-dark-blue').addClass('btn-wo-active');
+        });
+
+
+        // when btn-pop-up clicked 
+        $('#btn-pop-up').click(function() {
+            // check if there is active class
+            if ($('.btn-wo-active').length) {
+                var workOrderId = $('.btn-wo-active').data('id');
+
+                // show modal
+                $('#modal-work-order-mobile-menu').modal('show');
+            } else {
+                // show pop up
+                swal.fire({
+                    title: 'No Work Order Selected',
+                    text: 'Please select work order first',
+                    icon: 'warning',
+                    confirmButtonText: 'OK'
+                });
+            }
+        });
+
+        // on double click work order list
+        $('#lazy-load-list-data').on('dblclick', '#work-order-list', function() {
+            var workOrderId = $(this).data('id');
+            $('#work_order_id').val(workOrderId);
+
+            // show modal work order detail
+            $('#modal-work-order-detail').modal('show');
         });
     });
 
     function loadWorkOrderList() {
-        isLoading = true; // Set the flag to true indicating loading in progress
+        isLoading = true;
 
         var limit = $('#lazy-load-limit').val();
         var offset = $('#lazy-load-offset').val();
@@ -109,7 +282,7 @@
                         <div class="card bg-white mt-1" id="work-order-list" data-id="${workOrder.id}">
                         <div class="row mt-2 mb-2 left-2">
                             <div class="col-2">
-                                <button class="btn bg-dark-blue float-left btn-rounded text-center font-size-10 text-light">
+                                <button class="btn bg-dark-blue float-left btn-rounded text-center font-size-10 text-light" id="btn-work-order-card" data-id="${workOrder.id}">
                                     ${no++}
                                 </button>
                             </div>
@@ -149,9 +322,7 @@
                         </div>
                     `);
                 }
-                isLoading = false; // Reset the flag after the load is complete
-                // $('#lazy-load-list-data').append(response);
-                // $('#lazy-load-offset').val(parseInt(offset) + parseInt(limit));
+                isLoading = false;
             },
             error: function() {
                 // Remove loading animation
