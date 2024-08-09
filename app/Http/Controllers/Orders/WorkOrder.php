63ab6fcbd3a4bb14c43da8be0019212f431f7ef4
R4D3N G0Z4LL <radengozal@gmail.com>
2024-08-09 11:37:33 +0700

    Add trackingOrder method to WorkOrder controller

--- app/Http/Controllers/Orders/WorkOrder.php
+++ app/Http/Controllers/Orders/WorkOrder.php
@@ -467,4 +467,24 @@ class WorkOrder extends Controller
             return getResponseData(false);
         }
     }
+
+    /**
+     *  Get tracking data.
+     */
+    public function trackingOrder($id)
+    {
+        try {
+            $workOrderId = $id;
+            $tracking = TrackingModel::where('work_order_id', $workOrderId)->first();
+
+            // return view
+            return view('Orders.Tracking.index', compact('tracking'));
+        } catch (\Throwable $th) {
+            Log::error($th);
+            return response()->json([
+                'status' => false,
+                'message' => 'Failed to get tracking data. ' . $th->getMessage()
+            ]);
+        }
+    }
 }
