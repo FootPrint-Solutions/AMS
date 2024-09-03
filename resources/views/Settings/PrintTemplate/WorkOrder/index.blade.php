3455fa516c17ae21ce630cf625b1c9b2096bc7e8
R4D3N G0Z4LL <radengozal@gmail.com>
2024-09-03 09:13:50 +0700

    feat: update tinymce to allow upload image

--- resources/views/Settings/PrintTemplate/WorkOrder/index.blade.php
+++ resources/views/Settings/PrintTemplate/WorkOrder/index.blade.php
@@ -77,12 +77,46 @@
                 plugins: [
                     'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
                     'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
-                    'insertdatetime', 'media', 'table', 'help', 'wordcount', 'importword'
+                    'insertdatetime', 'media', 'table', 'help', 'wordcount', 'importword', 'image code'
                 ],
                 toolbar: 'undo redo | blocks | ' +
                     'bold italic backcolor | alignleft aligncenter ' +
                     'alignright alignjustify | bullist numlist outdent indent | ' +
-                    'removeformat | help',
+                    'removeformat | help | undo redo | image code',
+                file_picker_types: 'image',
+                automatic_uploads: true,
+                file_picker_types: 'image',
+                file_picker_callback: (cb, value, meta) => {
+                    const input = document.createElement('input');
+                    input.setAttribute('type', 'file');
+                    input.setAttribute('accept', 'image/*');
+
+                    input.addEventListener('change', (e) => {
+                        const file = e.target.files[0];
+
+                        const reader = new FileReader();
+                        reader.addEventListener('load', () => {
+                            /*
+                              Note: Now we need to register the blob in TinyMCEs image blob
+                              registry. In the next release this part hopefully won't be
+                              necessary, as we are looking to handle it internally.
+                            */
+                            const id = 'blobid' + (new Date()).getTime();
+                            const blobCache = tinymce.activeEditor.editorUpload.blobCache;
+                            const base64 = reader.result.split(',')[1];
+                            const blobInfo = blobCache.create(id, file, base64);
+                            blobCache.add(blobInfo);
+
+                            /* call the callback and populate the Title field with the file name */
+                            cb(blobInfo.blobUri(), {
+                                title: file.name
+                            });
+                        });
+                        reader.readAsDataURL(file);
+                    });
+
+                    input.click();
+                },
                 content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:16px }',
                 init_instance_callback: function(editor) {
                     editor.on('init', function() {
