aec29db84f8b3de3be8a4f4f3e3b8905ed883d5e
R4D3N G0Z4LL <radengozal@gmail.com>
2025-02-28 13:50:46 +0700

    refactor: reduce random battery selection limit from 12 to 8

--- app/Http/Controllers/Api/Battery.php
+++ app/Http/Controllers/Api/Battery.php
@@ -24,7 +24,7 @@ class Battery extends Controller
     function getRandomBattery(Request $request)
     {
         try {
-            $battery = BatteryModel::inRandomOrder()->limit(12)->with('batteryUrl')->get()->toArray();
+            $battery = BatteryModel::inRandomOrder()->limit(8)->with('batteryUrl')->get()->toArray();
             $response = [
                 'status' => 'success',
                 'message' => 'Data found',
