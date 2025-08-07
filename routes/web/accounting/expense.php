3cfb6511199c4a35be302355997182290a7a5b5d
R4D3N G0Z4LL <radengozal@gmail.com>
2025-08-07 11:46:09 +0700

    Update expense routes to use more specific permission names

--- routes/web/accounting/expense.php
+++ routes/web/accounting/expense.php
@@ -4,11 +4,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Accounting\Expense;
 
 
-Route::get('/expense/', [Expense::class, 'index'])->name('expense.index')->middleware('permission:view_expense');
-Route::post('/expense/show', [Expense::class, 'show'])->name('expense.show')->middleware('permission:view_expense');
-Route::get('/expense/create', [Expense::class, 'create'])->middleware('permission:add_expense');
-Route::get('/expense/edit/{id}', [Expense::class, 'edit'])->name('expense.edit')->middleware('permission:edit_expense');
-Route::post('/expense/store', [Expense::class, 'store'])->name('expense.store')->middleware('permission:add_expense');
-Route::post('/expense/update', [Expense::class, 'update'])->name('expense.update')->middleware('permission:edit_expense');
-Route::post('/expense/destroy', [Expense::class, 'destroy'])->name('expense.destroy')->middleware('permission:delete_expense');
-Route::post('/expense/toggle', [Expense::class, 'toggle'])->name('expense.toggle')->middleware('permission:edit_expense');
+Route::get('/expense/', [Expense::class, 'index'])->name('expense.index')->middleware('permission:view_expense_data');
+Route::post('/expense/show', [Expense::class, 'show'])->name('expense.show')->middleware('permission:view_expense_data');
+Route::get('/expense/create', [Expense::class, 'create'])->middleware('permission:add_expense_data');
+Route::get('/expense/edit/{id}', [Expense::class, 'edit'])->name('expense.edit')->middleware('permission:edit_expense_data');
+Route::post('/expense/store', [Expense::class, 'store'])->name('expense.store')->middleware('permission:add_expense_data');
+Route::post('/expense/update', [Expense::class, 'update'])->name('expense.update')->middleware('permission:edit_expense_data');
+Route::post('/expense/destroy', [Expense::class, 'destroy'])->name('expense.destroy')->middleware('permission:delete_expense_data');
+Route::post('/expense/toggle', [Expense::class, 'toggle'])->name('expense.toggle')->middleware('permission:edit_expense_data');
